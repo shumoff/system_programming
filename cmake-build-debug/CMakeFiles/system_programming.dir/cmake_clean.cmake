ab45e2ba5d95d2d25ad0461f980f03d8772c6dce
shumoff <dmitry.shumoff@gmail.com>
2020-02-06 20:13:01 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/system_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/system_programming.dir/main.cpp.o"
+  "system_programming"
+  "system_programming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/system_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/system_programming.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for system_programming.
+# This may be replaced when dependencies are built.
